c1e6e9d7d897d4e4795c13af1bb373dc24f4256e
DevZua <gimzua0228@gmail.com>
2024-01-09 15:58:02 +0900

    first commit

---
+++ src/test/java/org/example/supercoding/SupercodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.supercoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
